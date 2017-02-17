2e81f928454c8838a034763ad6865b2d5008efd7
Justyna <justynazarudzka@gmail.com>
2017-02-17 20:32:04 +0100

    Add files via upload

--- js/move-items.js
+++ js/move-items.js
@@ -12,30 +12,29 @@ function itemClickEventHandler(e) {
 
 var main = function () {
     $(document).keypress(function (event) {
-      if (event.which === 60) {
-        var currentItem = $('.current');
-        var previousItem = currentItem.prev();
-        if (previousItem.length === 0) {
+      var currentItem = $('.current');
+      var previousItem = currentItem.prev();
+      if (previousItem.length === 0) {
           previousItem = $('.item').last();
-        }
+      }
+      
+      var nextItem = currentItem.next();
+      if (nextItem.length === 0) {
+         nextItem = $('.item').first();
+      }
+      
+      if (event.which === 60) {
+        
         currentItem.removeClass('current');
         previousItem.addClass('current');
         
       } else if (event.which === 62) {
-        var currentItem = $('.current');
-        var nextItem = currentItem.next();
-        if (nextItem.length === 0) {
-          nextItem = $('.item').first();
-        }
+        
         currentItem.removeClass('current');
         nextItem.addClass('current');
         
       } else if (event.which === 42) {
-        var currentItem = $('.current');
-        var nextItem = currentItem.next();
-        if (nextItem.length === 0) {
-          nextItem = $('.item').first();
-        }
+        
         currentItem.addClass('done');
         currentItem.removeClass('current');
         nextItem.addClass('current');
