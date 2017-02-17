978bcf0fd2a9245e46963cb71584df7e34789381
Justyna <justynazarudzka@gmail.com>
2017-02-17 09:51:31 +0100

    Add files via upload

--- js/move-items.js
+++ js/move-items.js
@@ -1,16 +1,43 @@
 var main = function () {
+    $('.item').click(function () {
+      $('.item').removeClass('current');
+      $(this).toggleClass('current');
+      
+    });
+  
     $(document).keypress(function (event) {
-      if (event.which === 80) {
+      if (event.which === 60) {
         var currentItem = $('.current');
         var previousItem = currentItem.prev();
+        if (previousItem.length === 0) {
+          previousItem = $('.item').last();
+        }
         currentItem.removeClass('current');
         previousItem.addClass('current');
-      } else if (event.which === 78) {
+        
+      } else if (event.which === 62) {
         var currentItem = $('.current');
         var nextItem = currentItem.next();
+        if (nextItem.length === 0) {
+          nextItem = $('.item').first();
+        }
         currentItem.removeClass('current');
         nextItem.addClass('current');
+        
+      } else if (event.which === 42) {
+        var currentItem = $('.current');
+        var nextItem = currentItem.next();
+        if (nextItem.length === 0) {
+          nextItem = $('.item').first();
+        }
+        currentItem.addClass('done');
+        currentItem.removeClass('current');
+        nextItem.addClass('current');
+        $('.done').remove();
+      } else if (event.which === 94) {
+        $('.current').addClass('progress');
       }
     });
   };
+  
 $(document).ready(main);
